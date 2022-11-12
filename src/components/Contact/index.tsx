f2c1a9b7f0b550b47fbd20029d1cb292b060f174
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2022-11-12 09:26:01 +0200

    Adding emailjs

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,17 +1,38 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
+import emailjs from "@emailjs/browser";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState<string>("text-animate");
-
+  const refForm = useRef<HTMLFormElement>(null);
   useEffect(() => {
     setTimeout(() => {
       setLetterClass("text-animate-hover");
     }, 3000);
   }, []);
 
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_hdpy0k9",
+        refForm.current || "#contactForm",
+        "8Pq-n5keHD028dVpT"
+      )
+      .then(() => {
+        alert("Sent successfully");
+        // window.location.reload(false);
+      })
+      .catch((r) => {
+        console.error(r);
+        alert("Failed to send, please try again");
+      });
+  };
   return (
     <>
       <div className="container contact-page">
@@ -25,7 +46,7 @@ const Contact = () => {
           </h1>
           <p>This is some lame text about me bla bla bla</p>
           <div className="contact-form">
-            <form>
+            <form ref={refForm} onSubmit={sendEmail} id="contactForm">
               <ul>
                 <li className="half">
                   <input type="text" name="name" placeholder="Name" required />
@@ -55,6 +76,23 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="map-wrap">
+          <div className="info-map">
+            Ahmed Sameh,
+            <br />
+            Egypt,
+            <br />
+            Cairo, Manial
+            <br />
+            <span>developer.ahmed99@gmail.com</span>
+          </div>
+          <MapContainer center={[44, 19]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44, 19]}>
+              <Popup>هنا يرقد الحزلقوم</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" active={true} />
